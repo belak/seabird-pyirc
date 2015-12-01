460d91307e2d3183cff8f81f457bd05d5be26f2e
Jay Vana <jsvana@mtu.edu>
2015-11-30 22:49:03 -0800

    Get weather description

--- seabird/weather.py
+++ seabird/weather.py
@@ -33,11 +33,20 @@ class WeatherPlugin(BaseExtension):
         try:
             data = resp.json()
 
+            # Once again, I am terribly lazy
+            desc = ''
+            try:
+                desc = '. {}'.format(data['weather'][0]['description'])
+            except:
+                # No description here
+                pass
+
             self.base.mention_reply(line, '{}: {:.2f} with a high of {:.2f} '
-                'and low of {:.2f}'.format(
+                'and low of {:.2f}{}.'.format(
                     data['name'],
                     self._kelvinToFahrenheit(data['main']['temp']),
                     self._kelvinToFahrenheit(data['main']['temp_max']),
-                    self._kelvinToFahrenheit(data['main']['temp_min'])))
+                    self._kelvinToFahrenheit(data['main']['temp_min']),
+                    desc))
         except:
             self.base.mention_reply(line, 'Got malformed weather response')
